cce38e9dd466dd278f75068c4a9d2fdf4faf233d
aliaksei <fezhenko@gmail.com>
2023-03-04 23:54:49 +0400

    add gitignore, fix checkstyle error

--- users-service/src/main/java/org/example/usersservice/controller/UsersController.java
+++ users-service/src/main/java/org/example/usersservice/controller/UsersController.java
@@ -12,7 +12,13 @@ import org.example.usersservice.dto.CreateUserResponseDto;
 import org.example.usersservice.model.AppUser;
 import org.example.usersservice.service.UsersService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import javax.validation.Valid;
 import java.util.List;
