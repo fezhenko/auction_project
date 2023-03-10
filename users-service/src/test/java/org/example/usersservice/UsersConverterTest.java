edeb2f2be886a27d5ccf072e07c6a8fc5f9a9131
aliaksei.fezhanka <extern.fezhanka_aliaksei@allianz.com>
2023-03-10 20:06:51 +0400

    add dependencies to pom of api-gateway service

--- users-service/src/test/java/org/example/usersservice/UsersConverterTest.java
+++ users-service/src/test/java/org/example/usersservice/UsersConverterTest.java
@@ -7,6 +7,7 @@ import org.example.usersservice.dto.users.UserPaymentsDto;
 import org.example.usersservice.model.AppUser;
 import org.example.usersservice.model.Payment;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,73 +17,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class UsersConverterTest {
-    @Test
-    @DisplayName("Verify the user is correctly converted from AppUser to AppUserDto")
-    @SneakyThrows
-    public void testConvertingFromAppUserToAppUserDto() {
-        UsersConverter usersConverter = new UsersConverter() {
-            @Override
-            public List<AppUserDto> toDto(List<AppUser> appUsers) {
-                return null;
-            }
 
-            @Override
-            public AppUserDto toDto(AppUser appUser) {
-                return AppUserDto.builder()
-                        .id(appUser.getId())
-                        .firstname(appUser.getFirstname())
-                        .lastname(appUser.getLastname())
-                        .email(appUser.getEmail())
-                        .role(appUser.getRole())
-                        .balance(appUser.getBalance())
-                        .phoneNumber(appUser.getPhoneNumber())
-                        .createdAt(appUser.getCreatedAt())
-                        .build();
-            }
+    private AppUser appUser;
+    private AppUserDto appUserDto;
+    private List<AppUser> appUsersList;
+    private List<AppUserDto> appUsersDtoList;
+    private UsersConverter usersConverter;
 
-            @Override
-            public List<UserPaymentsDto> paymentsToDto(List<Payment> payments) {
-                return null;
-            }
-
-            @Override
-            public UserPaymentsDto paymentsToDto(Payment payments) {
-                return null;
-            }
-        };
-
-        final AppUser appUser = new AppUser(
-                123123L,
-                "firstName",
-                "lastName",
-                "email@test.test",
-                "password1",
-                "USER",
-                100D,
-                "1234567890",
-                new Date(3000)
-        );
-
-        final AppUserDto expectedResult =
-                AppUserDto.builder()
-                        .id(123123L)
-                        .firstname("firstName")
-                        .lastname("lastName")
-                        .email("email@test.test")
-                        .role("USER")
-                        .balance(100D)
-                        .phoneNumber("1234567890")
-                        .createdAt(new Date(3000))
-                        .build();
-
-        Assertions.assertEquals(expectedResult, usersConverter.toDto(appUser));
-    }
-
-    @Test
-    @DisplayName("Verify the list of users is correctly converted from AppUser to AppUserDto")
-    @SneakyThrows
-    public void testConvertingFromListAppUserToAppUserDto() {
-        UsersConverter usersConverter = new UsersConverter() {
+    @BeforeEach
+    public void init() {
+        usersConverter = new UsersConverter() {
             @Override
             public List<AppUserDto> toDto(List<AppUser> appUsers) {
                 return appUsers.stream()
@@ -125,10 +69,10 @@ public class UsersConverterTest {
             }
         };
 
-        List<AppUser> appUsersList = new ArrayList<>();
-        List<AppUserDto> appUsersDtoList = new ArrayList<>();
-        AppUser appUser1 = new AppUser(
-                123121L,
+        appUsersList = new ArrayList<>();
+        appUsersDtoList = new ArrayList<>();
+        appUser = new AppUser(
+                123123L,
                 "firstName",
                 "lastName",
                 "email@test.test",
@@ -138,22 +82,9 @@ public class UsersConverterTest {
                 "1234567890",
                 new Date(3000)
         );
-        AppUser appUser2 = new AppUser(
-                123122L,
-                "firstName",
-                "lastName",
-                "email@test.test",
-                "password1",
-                "USER",
-                100D,
-                "1234567890",
-                new Date(3000)
-        );
-        appUsersList.add(appUser1);
-        appUsersList.add(appUser2);
-        AppUserDto appUserDto1 =
+        appUserDto =
                 AppUserDto.builder()
-                        .id(123121L)
+                        .id(123123L)
                         .firstname("firstName")
                         .lastname("lastName")
                         .email("email@test.test")
@@ -162,19 +93,21 @@ public class UsersConverterTest {
                         .phoneNumber("1234567890")
                         .createdAt(new Date(3000))
                         .build();
-        AppUserDto appUserDto2 =
-                AppUserDto.builder()
-                        .id(123122L)
-                        .firstname("firstName")
-                        .lastname("lastName")
-                        .email("email@test.test")
-                        .role("USER")
-                        .balance(100D)
-                        .phoneNumber("1234567890")
-                        .createdAt(new Date(3000))
-                        .build();
-        appUsersDtoList.add(appUserDto1);
-        appUsersDtoList.add(appUserDto2);
+        appUsersList.add(appUser);
+        appUsersDtoList.add(appUserDto);
+    }
+
+    @Test
+    @DisplayName("Verify the user is correctly converted from AppUser to AppUserDto")
+    @SneakyThrows
+    public void testConvertingFromAppUserToAppUserDto() {
+        Assertions.assertEquals(appUserDto, usersConverter.toDto(appUser));
+    }
+
+    @Test
+    @DisplayName("Verify the list of users is correctly converted from AppUser to AppUserDto")
+    @SneakyThrows
+    public void testConvertingFromListAppUserToAppUserDto() {
         Assertions.assertEquals(appUsersDtoList, usersConverter.toDto(appUsersList));
     }
 
